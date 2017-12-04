2b07daa7586dcf2f0d889d52579b517943a9dc97
lorrsiochi <33431340+lorrsiochi@users.noreply.github.com>
2017-12-04 14:31:31 -0500

    Update and rename initial_data.ipynb to initial_data.py

--- initial_data.py
+++ initial_data.py
@@ -6,6 +6,8 @@
 
 #This program will receive the initial data to create a budget tracker.
 
+import pandas as pd
+
 print("Welcome to your personal budget tracker!")
 
 first_name = input("Enter first name: ")
@@ -14,3 +16,7 @@ last_name = input("Enter last name: ")
 income_str = input("How many incomes do you have?")
 income_num = int(income_str)
 
+while income_num > 0:
+    income_name = input("Enter the name of the income source: ")
+    income_amt = input("How much do you make per month with this income?: ")
+    income_num -= income_num
