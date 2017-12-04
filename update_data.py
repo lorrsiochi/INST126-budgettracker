22ec29a0d890e80b820fd964b3a02a0ff0518dd7
lorrsiochi <33431340+lorrsiochi@users.noreply.github.com>
2017-12-04 18:10:17 -0500

    Create update_data.py

---
+++ update_data.py
@@ -0,0 +1,40 @@
+#need to link this to the csv file 
+"""
+    from csv file, we need their total budget
+    
+    All incomes added together
+  - Bills
+  -------------------------------
+    total budget
+"""
+    
+
+first_name = ("Enter your first name: ")
+last_name = ("Enter your last name: ")
+
+print("Menu Selection")
+print("1 = Add expense")
+print("2 = Remove expense")
+print("3 = Add Revenue")
+print("4 = Remove revenue")
+print("5 = Exit")
+
+choice_str = input("Enter selection: ")
+choice_int = int(choice_str)
+
+if choice_int == 1:
+    """
+        ask for amount of expense
+        ask for category (possibly display the suggested categories)
+        date of expense
+    """
+elif choice_int == 2:
+
+elif choice_int == 3:
+    
+elif choice_int == 4:
+    
+elif choice_int == 5:
+    print("Goodbye!")
+else:
+    print("That is not a valid choice. Please enter a number 1-5")
