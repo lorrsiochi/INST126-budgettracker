8bfce31e5dadaec826fb50466ee202d74afb5bf4
lorrsiochi <33431340+lorrsiochi@users.noreply.github.com>
2017-12-08 14:27:19 -0500

    Update initial_data.py

--- initial_data.py
+++ initial_data.py
@@ -13,49 +13,58 @@ list_bill_amt = []
 list_bill_occur = []
 list_bill_date = []
 
-#Trying to create eh dataframe
+#Trying to create a dataframe
+df = pd.read_csv("budget_tracker_files.csv")
 
 def income():
-    income_amt = input("How much revenue to receive each month?: $")
+    income_amt = input("How much revenue do you receive each month?: $")
     
     if income_amt.isdigit() == True:
         print("Thank you!")
         income_amt = float(income_amt)
         list_income_amt.append(income_amt)
+        df['Income Amount'] = income_amt
             
     else:
         print("Uh oh, that was not a vaild entry. Please try again.")
         income()
 
-        
+df = pd.read_csv('budget_tracker_files.csv')
 
 first_name = input("Enter first name: ")
 last_name = input("Enter last name: ")
 
+
+
 list_first_name.append(first_name.title())
 list_last_name.append(last_name.title())
+
+df['First Name'] = list_first_name
+df['Last Name'] = list_last_name
 
 print("\nHello "+ first_name.title() + " " + last_name.title() + "!")
 income()
 
-print(list_first_name)
-print(list_last_name)
-print(list_income_amt)
-    
-df = pd.DataFrame
-(
-    {"First Name": list_first_name,
-    "Last Name": list_last_name,
-    "Income Amt":list_income_amt}
-)
-
 
 num_bill = int(input("How many bills do you pay per month?: "))
-list_num_bill.append(num_bill)
 
 while num_bill > 0:
     bill_name = input("Enter the name of the bill: ")
+    list_bill_name.append(bill_name)
     bill_amt = float(input("How much do owe for this bill?: "))
-    bill_occur = int(input("How often does this bill occur (in days): "))
-    bill_date = input("When do you pay this bill (MM/DD): ")
-    num_bill -= 1
+    list_bill_amt.append(bill_amt)   
+    num_bill = num_bill -1
+    
+print(list_bill_name)
+print(list_bill_amt)
+    
+for name in list_bill_name:
+    counter = 0
+    column = 'Bill Name '+ str(counter + 1)
+    df[column] = name
+    counter += 1
+    
+for amt in list_bill_amt:
+    counter = 0
+    df['Bill ' + str(counter + 1) + 'Amt'] = amt
+    counter += 1
